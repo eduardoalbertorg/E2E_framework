0ef4e8510342b8066d87e846f216e4da72c9ba01
Eduardo Rodriguez <eduardoalbertorg@gmail.com>
2022-03-16 09:33:45 -0600

    Parameterize data

--- src/test/java/Academy/HomePage.java
+++ src/test/java/Academy/HomePage.java
@@ -2,6 +2,7 @@ package Academy;
 
 import java.io.IOException;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pageObjects.LandingPage;
@@ -10,17 +11,33 @@ import resources.Base;
 
 public class HomePage extends Base {
 	
-	@Test
-	public void basePageNavigation() throws IOException {
+	@Test(dataProvider = "getData")
+	public void basePageNavigation(String Username, String Password, String text) throws IOException {
 		driver = initializeDriver();
 		driver.get("http://qaclickacademy.com");
 		LandingPage landing = new LandingPage(driver);
 		LoginPage login = new LoginPage(driver);
 		landing.getNoThanksButton().click();
 		landing.getLogin().click();
-		login.getEmail().sendKeys("abc@qw.com");
-		login.getPassword().sendKeys("12345");
+		login.getEmail().sendKeys(Username);
+		login.getPassword().sendKeys(Password);
 		login.getLoginButton().click();
+		driver.quit();
+	}
+	
+	@DataProvider
+	public Object[][] getData() {
+		// Row is for how many different data types test should run
+		// column stands for how many values per each test
+		Object[][] data = new Object[2][3];
+		data[0][0] = "nonrestricteduser@qw.com";
+		data[0][1] = "123456";
+		data[0][2] = "Restricted User";
+		
+		data[1][0] = "restricteduser@qw.com";
+		data[1][1] = "456788";
+		data[1][2] = "Non restrictec user";
+		return data;
 	}
 
 }
