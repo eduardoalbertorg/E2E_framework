1bf8d591f64ddc7d42844f4ac130f031d9d5084b
Eduardo Rodriguez <eduardoalbertorg@gmail.com>
2022-03-17 18:16:44 -0600

    Convert framework into TestNG

--- src/test/java/Academy/validateTitle.java
+++ src/test/java/Academy/validateTitle.java
@@ -4,20 +4,25 @@ import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
 import java.io.IOException;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 
 import pageObjects.LandingPage;
 import resources.Base;
 
 public class ValidateTitle extends Base {
-	@Test(dataProvider = "getData")
-	public void basePageNavigation(String Username, String Password, String text) throws IOException {
+	@BeforeTest
+	public void intialize() throws IOException {
 		driver = initializeDriver();
 		driver.get(prop.getProperty("baseUrl"));
+	}
+	
+	@Test(dataProvider = "getData")
+	public void basePageNavigation(String Username, String Password, String text) throws IOException {
 		LandingPage landing = new LandingPage(driver);
 		String actualTitle = landing.getFeaturedCoursesTitle().getText();
-		AssertJUnit.assertEquals(actualTitle, "Featured Courses");
-		driver.quit();
+		AssertJUnit.assertEquals("featured courses", actualTitle.toLowerCase());
 	}
 	
 	@DataProvider
@@ -29,6 +34,11 @@ public class ValidateTitle extends Base {
 		data[0][1] = "123456";
 		data[0][2] = "Restricted User";
 		return data;
+	}
+	
+	@AfterTest
+	public void teardown() {
+		driver.close();
 	}
 
 }
